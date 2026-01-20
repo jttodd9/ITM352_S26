a3afe7a47cbfe88392f94b7f2e451a135f559a21
jttodd9 <jttodd@hawaii.edu>
2026-01-20 13:06:23 -1000

    ex1 lab 2

---
+++ Lab2/Ex1.py
@@ -0,0 +1,15 @@
+#Ask the user for a number between 1 and 100 and print out the square of that number.
+#name: Justin Todd
+#date: Jan 20, 2026
+
+
+print("Welcome to the program")
+value_entered = input("Please enter a whole number between 1 and 100: ")
+print("You entered: " + value_entered)
+
+value_as_int = int(value_entered)
+squared_value = value_as_int * value_as_int
+if value_as_int < 1 or value_as_int > 100:
+    print("The value entered is out of range.")
+else:
+    print(f"The square of {value_entered} is {squared_value}")
