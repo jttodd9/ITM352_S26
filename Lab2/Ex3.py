e0503c1881b17bd2e6151b2c5faf3687265414ce
jttodd9 <jttodd@hawaii.edu>
2026-01-20 19:08:57 -1000

    Lab 2 Ex3

---
+++ Lab2/Ex3.py
@@ -0,0 +1,6 @@
+# asks the user to enter a value 1-100 with a decimal and convets the input to a float then squares it.
+# name: Justin Todd
+# date: Jan 20, 2026
+
+print("Welcome to the program")
+value_entered = input("Please enter a number between 1 and 100 (decimals allowed): ")
